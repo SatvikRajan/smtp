c8949aa52ee2bf6338c0de7292572ef0bb138abc
Eshan Singh <progressivesingh@gmail.com>
2024-04-17 20:51:08 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'satvikrajan@gmail.com',
+        pass: 'cwlx qmia fpco ifij'
+    }
+});
+
+
+app.post('/send-email', (req, res) => {
+    const { senderName, senderEmail, subject, text } = req.body;
+
+
+    const mailOptions = {
+        from: `"${senderName}" <${senderEmail}>`,
+        to: 'eshan.singh.04.dev@gmail.com',
+        subject: subject,
+        text: `Sender: ${senderName} <${senderEmail}>\n\n${text}`
+    };
+
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).send('Email sent successfully');
+        }
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
